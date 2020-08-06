eb9c7e46eee76d9f8fd3fca66c181e9274b89a8c
phdfreitas <pedro.hnq.fraga@gmail.com>
2020-08-06 10:43:03 -0300

    Task list

--- js/tasks.js
+++ js/tasks.js
@@ -19,13 +19,42 @@ class Db{
 		localStorage.setItem(taskId, task)
 		localStorage.setItem('taskId', taskId)
 	}
+
+	getAllTasks(){
+		let tasks = []
+		let taskId = localStorage.getItem('taskId')
+
+		for (let i = 1; i <= taskId; i++) {
+			let task = localStorage.getItem(i)
+			
+			if(task !== null) tasks.push(task)
+		}
+
+		return tasks
+	}
 }
 
 let db = new Db()
 
 function addNewTask(){
-
+	
 	let task = document.getElementById('task')
-
 	db.newTask(task.value)
+}
+
+function loadTasks(){
+	let tasks = []
+	tasks = db.getAllTasks()
+
+	let taskList = document.getElementById('all_tasks')
+	
+	tasks.forEach( (t, index) => {
+		let row = taskList.insertRow()
+		let input = document.createElement('input')
+		input.setAttribute('type', 'checkbox')
+
+		row.insertCell(0).innerHTML = index + 1
+		row.insertCell(1).innerHTML = t
+		row.insertCell(2).appendChild(input)
+	});
 }

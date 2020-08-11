1a08a47b6679dffcb17fe6e9896384f015cfce61
phdfreitas <pedro.hnq.fraga@gmail.com>
2020-08-11 09:14:06 -0300

    Delete tasks

--- js/tasks.js
+++ js/tasks.js
@@ -27,10 +27,14 @@ class Db{
 		for (let i = 1; i <= taskId; i++) {
 			let task = localStorage.getItem(i)
 			
-			if(task !== null && task !== undefined && task !== '') tasks.push(task)
+			if(task !== null && task !== undefined && task !== '') tasks.push(`${task}${i}`)
 		}
 
 		return tasks
+	}
+
+	deleteTask(task){
+		localStorage.removeItem(task)
 	}
 }
 
@@ -51,14 +55,29 @@ function loadTasks(){
 	tasks = db.getAllTasks()
 
 	let taskList = document.getElementById('all_tasks')
-	
+
 	tasks.forEach( (t, index) => {
 		let row = taskList.insertRow()
 		let input = document.createElement('input')
 		input.setAttribute('type', 'checkbox')
 
 		row.insertCell(0).innerHTML = index + 1
-		row.insertCell(1).innerHTML = t
+		row.insertCell(1).innerHTML = t.slice(0, t.length - 1)
 		row.insertCell(2).appendChild(input)
+
+		let btn = document.createElement('span')
+		btn.innerHTML = 'clear'
+		btn.className = 'material-icons remove'
+		btn.style.cursor = 'pointer'
+		btn.style.lineHeight = 1.4
+
+		let id = t.slice(t.length - 1, t.length)
+
+		btn.onclick = function(){
+			db.deleteTask(id)
+			window.location.reload()
+		}
+
+		row.insertCell(3).appendChild(btn)
 	});
 }
